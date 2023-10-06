1469bedb6897dd8b669dc69a2137bab01d384e4c
MoniekNK <monika.nowakowska01@gmail.com>
2023-10-06 22:23:39 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 'use strict';
 
 import axios from 'axios';
+import Notiflix from 'notiflix';
+
 axios.defaults.headers.common['x-pixabay-key'] =
   '754704-15f293fdc79a851fbfbf7bf56';
-
-import Notiflix from 'notiflix';
 
 const searchForm = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreButton = document.getElementById('load-more');
 
-const API_KEY = '754704-15f293fdc79a851fbfbf7bf56';
 const BASE_URL = 'https://pixabay.com/api/';
+const perPage = 40;
+
 let currentPage = 1;
 let currentQuery = '';
 
@@ -48,43 +49,61 @@ loadMoreButton.addEventListener('click', async () => {
 });
 
 async function fetchImages(query, page) {
-  const response = await axios.get(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
-  );
-  const data = response.data;
-  return data.hits;
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?key=${axios.defaults.headers.common['x-pixabay-key']}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    );
+    const data = response.data;
+    return data.hits;
+  } catch (error) {
+    throw error;
+  }
 }
 
 function displayImages(images) {
-  images.forEach(image => {});
+  images.forEach(imageData => {
+    const photoCard = document.createElement('div');
+    photoCard.classList.add('photo-card');
 
-  if (images.length === 0) {
+    const imageElement = document.createElement('img');
+    imageElement.src = imageData.webformatURL;
+    imageElement.alt = imageData.tags;
+    imageElement.loading = 'lazy';
+
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('info');
+
+    const likes = document.createElement('p');
+    likes.classList.add('info-item');
+    likes.innerHTML = `<b>Likes:</b> ${imageData.likes}`;
+
+    const views = document.createElement('p');
+    views.classList.add('info-item');
+    views.innerHTML = `<b>Views:</b> ${imageData.views}`;
+
+    const comments = document.createElement('p');
+    comments.classList.add('info-item');
+    comments.innerHTML = `<b>Comments:</b> ${imageData.comments}`;
+
+    const downloads = document.createElement('p');
+    downloads.classList.add('info-item');
+    downloads.innerHTML = `<b>Downloads:</b> ${imageData.downloads}`;
+
+    infoDiv.appendChild(likes);
+    infoDiv.appendChild(views);
+    infoDiv.appendChild(comments);
+    infoDiv.appendChild(downloads);
+
+    photoCard.appendChild(imageElement);
+    photoCard.appendChild(infoDiv);
+
+    gallery.appendChild(photoCard);
+  });
+
+  if (images.length < perPage) {
     loadMoreButton.style.display = 'none';
-    Notiflix.Notify.failure(
+    Notiflix.Notify.Info(
       "We're sorry, but you've reached the end of search results."
     );
-  }
-
-  loadImages(currentQuery, currentPage);
-}
-
-async function loadImages(query, page) {
-  try {
-    const response = await axios.get(
-      `https://pixabay.com/api/?page=${page}&per_page=40&q=${query}`
-    );
-    const data = response.data;
-    const maxPages = Math.ceil(data.totalHits / 40);
-
-    if (currentPage >= maxPages) {
-      document.getElementById('load-more').style.display = 'none';
-      Notiflix.Notify.Info(
-        "We're sorry, but you've reached the end of search results."
-      );
-    } else {
-      document.getElementById('load-more').style.display = 'block';
-    }
-  } catch (error) {
-    console.error('Błąd podczas ładowania obrazków:', error);
   }
 }
